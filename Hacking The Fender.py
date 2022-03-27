af123050cb24e3610c38cc65f7e6abdee1e49048
Juanjo <100070731+VeganBurger@users.noreply.github.com>
2022-03-27 19:50:48 +0100

    Update Hacking The Fender.py

--- Hacking The Fender.py
+++ Hacking The Fender.py
@@ -1,6 +1,6 @@
-#Step 1 and 12
+#Steps 1 and 12
 import csv #Step 1
-import json #Step 12, always good practice to import modules on top of the file
+import json #Step 12, always good practice to import modules at the beginning of the file
 
 #Step 2
 compromised_users=[]
@@ -22,3 +22,25 @@ with open("compromised_users.txt", "w") as compromised_user_file:
 with open("boss_message.json", "w") as boss_message:
   boss_message_dict = {"recipient": "The Boss", "message": "Mission Success"}
   json.dump(boss_message_dict, boss_message)
+
+#Steps 16, 17, 18, 19
+with open("new_passwords.csv", "w") as new_passwords_obj:
+  slash_null_sig = """
+ _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/
+"""
+  new_passwords_obj.write(slash_null_sig)
